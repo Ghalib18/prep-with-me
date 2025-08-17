54e838e85ccaa10d5d792b08b28b74459f705290
Md Ghalib Hussain <131044959+Ghalib18@users.noreply.github.com>
2025-08-17 17:55:41 +0530

    Update Loader.jsx

--- app/dashboard/_component/Loader.jsx
+++ app/dashboard/_component/Loader.jsx
@@ -1,7 +1,7 @@
 const Loader = () => {
   return (
     <div className="flex-col gap-4 w-full flex items-center justify-center mt-40">
-      <div className="w-24 h-24 border-8 text-black text-4xl animate-spin border-gray-300 flex items-center justify-center border-t-black rounded-full"></div>
+      <div className="w-24 h-24 border-8 text-black text-4xl animate-spin border-gray-300 flex items-center justify-center border-t-blue rounded-full"></div>
     </div>
   );
 };
